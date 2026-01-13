7f405b75d14d66eb59b0fc26f67a19e4f920cefc
luarakelly <lua.kellly@gmail.com>
2026-01-13 11:58:00 +0200

    First commit for this course.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Class name has to be the same as the file name.
+ * 
+ * Javadoc (also capitalized as JavaDoc or javadoc) is an API documentation
+ * generator for the Java programming language.
+ * 
+ * Tässä tulostetaan konsolille
+ */
+public class Main {
+    // methods
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
