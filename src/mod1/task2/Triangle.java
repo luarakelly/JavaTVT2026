9daf9fe0a2aaf81d7d709c4f9b4b8c475a9d9f95
luarakelly <lua.kellly@gmail.com>
2026-01-14 13:26:10 +0200

    Added the package path.
    Removed the dynamic implementation and did a static implementation.

---
+++ src/mod1/task2/Triangle.java
@@ -0,0 +1,12 @@
+package mod1.task2;
+
+public class Triangle { // Print out a triangle pattern using asterisks and defined spaces.
+    public static void main(String[] args) {
+        // Static:
+        System.out.printf("%s\n", "     *");
+        System.out.printf("%s\n", "    ***");
+        System.out.printf("%s\n", "   *****");
+        System.out.printf("%s\n", "  *******");
+
+    }
+}
