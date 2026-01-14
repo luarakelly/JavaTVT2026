90726c7d6963d14481edfe9853d1fbaca36604c1
luarakelly <lua.kellly@gmail.com>
2026-01-14 13:26:39 +0200

    Added the package path.

---
+++ src/mod1/task3/SumOfThreeNumbers.java
@@ -0,0 +1,23 @@
+package mod1.task3;
+
+import java.util.Scanner;
+
+public class SumOfThreeNumbers { // Modify the program so that the program also prints the product and average of
+    // the three integers.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The product of the numbers is " + (first * second * third));
+        System.out.println("The average of the numbers is " + ((first + second + third) / 3));
+    }
+}
