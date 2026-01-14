01d5fcc462c4f9a0502ed6549c68cf722930f05f
luarakelly <lua.kellly@gmail.com>
2026-01-14 13:24:45 +0200

    Updated the file system implementation in order to keep the file names as the gives class names.

--- src/mod1/Task1.java
+++
@@ -1,7 +0,0 @@
-package mod1;
-
-public class Task1 { // Greet the user with a fixed message
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-}
--- src/mod1/Task4.java
+++
@@ -1,5 +0,0 @@
-package mod1;
-
-public class Task4 {
-
-}
